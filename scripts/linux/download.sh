358d282f6007d9962ed0c57744846aba33643ddd
ThatBen <thatbenbierens@gmail.com>
2024-06-18 14:05:29 +0200

    sets up download script for linux

---
+++ scripts/linux/download.sh
@@ -0,0 +1,9 @@
+curl -o codex-v0.1.0-linux-amd64.tar.gz -L https://github.com/codex-storage/nim-codex/releases/download/v0.1.0/codex-v0.1.0-linux-amd64.tar.gz
+tar -xvzf codex-v0.1.0-linux-amd64.tar.gz
+
+curl -o codex-prover-v0.1.0-linux-amd64.zip -L https://github.com/codex-storage/nim-codex/releases/download/v0.1.0/codex-prover-v0.1.0-linux-amd64.zip
+unzip codex-prover-v0.1.0-linux-amd64.zip
+
+chmod +x codex-v0.1.0-linux-amd64
+chmod +x codex-v0.1.0-prover-linux-amd64
+
