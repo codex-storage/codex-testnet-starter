01c75a12f7e758063114a35091520bc1247549a3
Ben <thatbenbierens@gmail.com>
2024-06-25 09:53:54 +0200

    Setting up for-codexers folder

--- scripts/windows/generate.bat
+++ scripts/windows/generate.bat
@@ -4,14 +4,7 @@ setlocal enabledelayedexpansion
 echo Generating private key...
 
 :: Use PowerShell to make the API call and process the response
-powershell -Command "& {
-    $response = Invoke-RestMethod -Uri 'https://api.blockcypher.com/v1/eth/main/addrs' -Method Post
-    $privateKey = $response.private
-    $address = $response.address
-    $privateKey | Out-File -FilePath '.\eth.key' -NoNewline
-    Write-Host ' * your private key has been saved to %cd%\eth.key'
-    Write-Host (' * your ethereum address is 0x' + $address)
-}"
+powershell -Command "& { $response = Invoke-RestMethod -Uri 'https://api.blockcypher.com/v1/eth/main/addrs' -Method Post; $privateKey = $response.private; $address = $response.address;  $privateKey | Out-File -FilePath '.\eth.key' -NoNewline; Write-Host ' * your private key has been saved to %cd%\eth.key'; Write-Host (' * your ethereum address is 0x' + $address); }"
 
 :: Set file permissions to read-only for the current user
 icacls .\eth.key /inheritance:r
