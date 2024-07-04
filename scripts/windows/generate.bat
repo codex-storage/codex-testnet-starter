58eb1f06ce08eb553d4bcb899e922c6ff36fc796
benbierens <thatbenbierens@gmail.com>
2024-07-04 10:12:42 +0200

    fixes generate script for windows

--- scripts/windows/generate.bat
+++ scripts/windows/generate.bat
@@ -4,10 +4,10 @@ setlocal enabledelayedexpansion
 echo Generating private key...
 
 :: Use PowerShell to make the API call and process the response
-powershell -Command "& { $response = Invoke-RestMethod -Uri 'https://api.blockcypher.com/v1/eth/main/addrs' -Method Post; $privateKey = $response.private; $address = $response.address;  $privateKey | Out-File -FilePath '.\eth.key' -NoNewline; Write-Host ' * your private key has been saved to %cd%\eth.key'; Write-Host (' * your ethereum address is 0x' + $address); }"
+powershell -Command "& { $response = Invoke-RestMethod -Uri 'https://api.blockcypher.com/v1/eth/main/addrs' -Method Post; $privateKey = $response.private; $address = $response.address;  $privateKey | Out-File -Encoding ASCII -FilePath '.\eth.key'; $address | Out-File -Encoding ASCII -FilePath '.\eth.address'; Write-Host ' * your private key has been saved to %cd%\eth.key'; Write-Host ' * your address has been saved to %cd%\eth.address'; Write-Host (' * your ethereum address is 0x' + $address); }"
 
 :: Set file permissions to read-only for the current user
 icacls .\eth.key /inheritance:r
-icacls .\eth.key /grant:r %USERNAME%:R
+icacls .\eth.key /grant:r %USERNAME%:F
 
 exit /b 0
