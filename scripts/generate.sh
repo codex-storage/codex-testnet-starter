146ce23c10bdc23beb802ebad79f73065d36b9b6
Slava <20563034+veaceslavdoina@users.noreply.github.com>
2024-07-30 20:50:20 +0300

    Switch key generation to own service (https://github.com/codex-storage/infra-codex/issues/199) (#22)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+# Variables
+key_file="eth.key"
+address_file="eth.address"
+
+# Generate
 echo "Generating private key..."
 
-response=$(curl -sX POST https://api.blockcypher.com/v1/eth/main/addrs)
-echo -n "${response}" | grep -o '"private":.*"' | cut -d'"' -f4 > ./eth.key
-echo -n "${response}" | grep -o '"address":.*"' | cut -d'"' -f4 > ./eth.address
-chmod 600 ./eth.key
+response=$(curl -s https://key.codex.storage)
+awk -F ': ' '/private/ {print $2}' <<<"${response}" >"${key_file}"
+awk -F ': ' '/address/ {print $2}' <<<"${response}" >"${address_file}"
 
-# Read the address from the file
-address=$(cat ./eth.address)
+# Permissions
+chmod 600 "${key_file}"
 
-echo " * your private key has been saved to ${PWD}/eth.key"
-echo " * your ethereum address has been saved to ${PWD}/eth.address"
-echo " * your ethereum address is 0x${address}"
+# Show
+address=$(cat ${address_file})
+echo " * your private key has been saved to ${PWD}/${key_file}"
+echo " * your ethereum address has been saved to ${PWD}/${address_file}"
+echo " * your ethereum address is ${address}"
