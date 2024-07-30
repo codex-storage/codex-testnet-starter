b3685b6b55ba7f1f0a41e75b87cb636cb9fea66d
Slava <20563034+veaceslavdoina@users.noreply.github.com>
2024-07-30 20:41:42 +0300

    Switch key generation to own service (https://github.com/codex-storage/infra-codex/issues/199)

--- scripts/windows/generate.bat
+++ scripts/windows/generate.bat
@@ -1,13 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Variables
+set key_file="%cd%\eth.key"
+set address_file="%cd%\eth.address"
+
+:: Generate
 echo Generating private key...
 
 :: Use PowerShell to make the API call and process the response
-powershell -Command "& { $response = Invoke-RestMethod -Uri 'https://api.blockcypher.com/v1/eth/main/addrs' -Method Post; $privateKey = $response.private; $address = $response.address;  $privateKey | Out-File -Encoding ASCII -FilePath '.\eth.key'; $address | Out-File -Encoding ASCII -FilePath '.\eth.address'; Write-Host ' * your private key has been saved to %cd%\eth.key'; Write-Host ' * your address has been saved to %cd%\eth.address'; Write-Host (' * your ethereum address is 0x' + $address); }"
+powershell -Command "& { $response = Invoke-RestMethod -Uri 'https://key.codex.storage/json'; $privateKey = $response.private; $address = $response.address; $privateKey | Out-File -Encoding ASCII -FilePath '%key_file%'; $address | Out-File -Encoding ASCII -FilePath '%address_file%'; Write-Host ' * your private key has been saved to %key_file%'; Write-Host ' * your address has been saved to %address_file%'; Write-Host (' * your ethereum address is ' + $address); }"
 
 :: Set file permissions to read-only for the current user
-icacls .\eth.key /inheritance:r
-icacls .\eth.key /grant:r %USERNAME%:F
+icacls %cd%\%key_file% /inheritance:r >nul 2>&1
+icacls %cd%\%key_file% /grant:r %USERNAME%:F >nul 2>&1
 
 exit /b 0
